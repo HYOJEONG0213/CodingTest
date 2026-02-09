d1e02218fac75d99ebbc6f7d4d29427e8df0a506
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-02-09 14:49:50 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.cc
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.cc
@@ -1,35 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a, N, M, dp[104][104];
+int a, c, N, M;
 vector <int> A, B, ret;
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
 	for(int i = 1; i <= N; i++){
-		cin >> a;
-		A.push_back(a);
+		cin >> c;
+		A.push_back(c);
 	}
 	cin >> M;
 	for(int i = 1; i <= M; i++){
-		cin >> a;
-		B.push_back(a);
+		cin >> c;
+		B.push_back(c);
 	}
 	
 	
-	int i = 0, j = 0;
-	while(max_element(A.begin()+i, A.end()) != A.end()){
-		auto Aiter = max_element(A.begin()+i, A.end());
-		auto Biter = find(B.begin()+j, B.end(), *Aiter);
-		if(Biter != B.end()){
-			ret.push_back(*Biter);
-			i = Aiter - A.begin();
-			j = Biter - B.begin() + 1;
+	int aidx = 0, bidx = 0;
+	while(aidx < N && bidx < M){
+		int a = aidx, b = bidx, mx = 0;
+		for(int i = aidx; i < N; i++){
+			for(int j = bidx; j < M; j++){
+				if(A[i]==B[j] && mx < A[i]){
+					mx = A[i];
+					a = i;
+					b = j;
+				}
+			}
 		}
 		
-		A.erase(A.begin() + (Aiter - A.begin()));
-		continue;
+		if(mx == 0) break;
+		ret.push_back(mx);
+		aidx = a + 1;
+		bidx = b + 1;
 	}
 	
 	
