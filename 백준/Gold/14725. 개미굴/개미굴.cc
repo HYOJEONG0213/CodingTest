c7b7af39b91f46640849a515e4d3885c0882ad2e
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-02-10 16:27:50 +0900

    [Gold III] Title: 개미굴, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.cc
+++ 백준/Gold/14725. 개미굴/개미굴.cc
@@ -2,56 +2,32 @@
 using namespace std;
 
 int N, K;
-char a;
-string st[1004];
-
-struct Trie{
-	map<string, Trie *> next;
-	
-	~Trie(){
-		for(auto i : next){
-			if(i.second != nullptr) delete i.second;
-		}
-	}
-	
-	//trie 
-	void insert(const vector <string> & now, int index){
-		if(index >= now.size()) return;
-		
-		auto current = next.find(now[index]);
-		if(current == next.end()){
-			next[now[index]] = new Trie();
-		}
-		next[now[index]]->insert(now, index+1);
-	}
-	
-	//dfs
-	void find(int depth){
-		for(auto n : next){
-			// depth 깊이별로 탐색  
-			for(int i = 0; i < depth; i++){
-				cout << "--";
-			}
-			cout << n.first << "\n";
-			n.second -> find(depth+1);
-		}
-	}
-
-};
-
+string a;
+set <string> st;
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
-	Trie * trie = new Trie();
 	for(int i = 0; i < N; i++){
 		cin >> K;
-		vector <string> ch(K) ;
+		// 해당 b에 누적해야함  
+		string b = "";
 		for(int j = 0; j < K; j++){
-			cin >> ch[j];
+			cin >> a;
+			b += "*" + a;
+			st.insert(b);
 		}
-		trie->insert(ch, 0);
 	}
 	
-	trie->find(0);
+	for(auto value : st){
+		// 몇개의 문자열이 들어가있는가  
+		int cnt = count(value.begin(), value.end(), '*');
+		// 마지막 string만 추출  
+		int pos = value.find_last_of("*");
+		string temp = value.substr(pos+1); 
+		
+		
+		for(int depth = 0; depth < cnt-1; depth++) cout << "--";
+		cout << temp << "\n";
+	}
 }
