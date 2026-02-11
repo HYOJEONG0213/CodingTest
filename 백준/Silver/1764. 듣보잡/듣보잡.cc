3cee0707a808d15aef87e9ab01db2a675d6656ea
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-02-12 02:40:36 +0900

    [Silver IV] Title: 듣보잡, Time: 36 ms, Memory: 7580 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -7,6 +7,8 @@ set <string> st;
 vector <string> ret;
 
 int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
 	cin >> N >> M;
 	for(int i = 0; i < N; i++){
 		cin >> s;
