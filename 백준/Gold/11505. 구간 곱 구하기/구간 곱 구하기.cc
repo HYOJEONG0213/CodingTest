f1ee8546330fcf520e32b92a73b8e6813cbbdd2f
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-02-20 14:14:29 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 88 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
@@ -3,88 +3,56 @@ using namespace std;
 typedef long long ll;
 const ll mod = 1000000007;
 
-ll N, M, K, a,b,c, A[1000004], tree[1000004], temptree[1000004];
+ll N, M, K, a,b,c, A[1000004], tree[1000004*4];
 
-ll power(ll num, ll exp){
-	ll ret = 1;
-	num %= mod;
+
+ll init(ll s, ll e, ll index){
+	if(s == e) return tree[index] = A[s];
 	
-	while(exp > 0){
-		if(exp % 2 == 1) ret = (ret*num) % mod;
-		num = (num*num) % mod;
-		exp /= 2;
-	}
-	return ret;
-} 
+	ll mid = (s+e)/2;
+	ll l = init(s, mid, index*2);
+	ll r = init(mid+1, e, index*2+1);
+	return tree[index] = (l*r) % mod;
+}
 
-void update(ll index, ll value, int flag){
-	if(value == 0){
-		while(index <= N){
-			// flag가 *라면 0 늘려주고, /라면 0을 제거할거야~  
-			temptree[index] += flag;
-			index += index & -index;
-		}
-		return;
-	}
+ll update(ll s, ll e, ll index, ll target, ll value){
+	if(target < s || target > e) return tree[index];
+	if(s == e) return tree[index] = value;
 	
-	while(index <= N){
-		if(flag==1){
-			tree[index] *= value;
-			tree[index] %= mod;
-		}
-		else{
-			tree[index] *= power(value, mod-2);
-			tree[index] %= mod;
-		}
-		index += index & -index;
-	}
+	ll mid = (s+e)/2;
+	ll l = update(s, mid, index*2, target, value);
+	ll r = update(mid+1, e, index*2+1, target, value);
+	return tree[index] = (l*r) % mod;
 }
 
-ll sum(ll index){
-	ll ret = 1;
-	while(index > 0){
-		ret *= tree[index];
-		ret %= mod;
-		index -= index & -index;
-	}
-	return ret;
+ll sum(ll s, ll e, ll left, ll right, ll index){
+	if(e < left || s > right) return 1;
+	if(left <= s && e <= right) return tree[index];
+	
+	ll mid = (s+e)/2;
+	ll l = sum(s, mid, left, right, index*2);
+	ll r = sum(mid+1, e, left, right, index*2+1);
+	return (l*r) % mod;
 }
 
-ll sum_zero(ll index){
-	ll ret = 0;
-	while(index > 0){
-		ret += temptree[index];
-		index -= index & -index;
-	}
-	return ret;
-}
 
-int main(){
+
+int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	fill(tree, tree+1000004, 1);
 	cin >> N >> M >> K;
-	for(int i = 1; i <= N; i++){
+	for(int i = 1; i <= N; i++) {
 		cin >> A[i];
-		update(i, A[i], 1);
 	}
-	
-	for(int i = 0; i < M+K; i++){
+	init(1, N, 1);
+
+	for(int i = 0; i < M+K; i++) {
 		cin >> a >> b >> c;
-		if(a==1){
-			update(b, A[b], -1);
-			update(b, c, 1);
+		if(a==1) {
+			update(1, N, 1, b, c);
 			A[b] = c;
-		}
-		else{
-			ll zero = sum_zero(c) - sum_zero(b-1);
-			if(zero){
-				cout << 0 << "\n";
-				continue;
-			}
-			
-			int temp = sum(c);
-			int temp2 = sum(b-1);
-			cout << (temp * power(temp2, mod-2) % mod) << "\n";
+		} else {
+			cout << sum(1, N, b, c, 1) << "\n";
 		}
 	}
 }
