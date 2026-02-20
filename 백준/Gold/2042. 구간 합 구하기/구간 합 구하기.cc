505f4c8d14204b755fa04d253849e814e4342190
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-02-21 00:23:26 +0900

    [Gold I] Title: 구간 합 구하기, Time: 188 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -2,22 +2,41 @@
 using namespace std;
 typedef long long ll;
 
-ll a, b, c, N, M, K, A[1000004], tree[1000004];
+ll a, b, c, N, M, K, A[1000004], tree[4000004];
 
-void update(ll index, ll value){
-	while(index <= N){
+
+ll init(ll s, ll e, ll index){
+	if(s == e) return tree[index] = A[s];
+	
+	ll mid = (s+e)/2;
+	ll l = init(s, mid, index*2);
+	ll r = init(mid+1, e, index*2+1);
+	
+	return tree[index] = l+r;
+}
+
+ll sum(ll s, ll e, ll left, ll right, ll index){
+	if(e < left || s > right) return 0;
+	if(left <= s && right >= e) return tree[index];
+	
+	
+	ll mid = (s+e)/2;
+	ll l = sum(s, mid, left, right, index*2);
+	ll r = sum(mid+1, e, left, right, index*2+1);
+	return l + r;
+}
+
+void update(ll s, ll e, ll index, ll target, ll value){
+	if(target < s || target > e) return;
+	if(s==e) {
 		tree[index] += value;
-		index += index & -index;
+		return;
 	}
-} 
-
-ll sum(ll index){
-	ll ret = 0;
-	while(index > 0){
-		ret += tree[index];
-		index -= index & -index;
-	}
-	return ret;
+	tree[index] += value;
+	
+	ll mid = (s+e)/2;
+	update(s, mid, index*2, target, value);
+	update(mid+1, e, index*2+1, target, value);
 }
 
 int main() {
@@ -25,17 +44,18 @@ int main() {
 	cin >> N >> M >> K;
 	for(ll i = 1; i <= N; i++){
 		cin >> A[i];
-		update(i, A[i]);
 	}
+	init(1, N, 1);
+	
 	for(ll i = 0; i < M + K; i++){
 		cin >> a >> b >> c;
 		if(a == 1){
 			ll temp = c - A[b];
-			update(b, temp);
 			A[b] = c;
+			update(1, N, 1, b, temp);
 		}
 		else{
-			cout << sum(c) - sum(b-1) << "\n";
+			cout << sum(1, N, b, c, 1) << "\n";
 		}
 	}	
 }
