ac270f18a246c2c73dc8b45eb1f4a6a414b3b19d
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-01 14:09:14 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 152 ms, Memory: 99676 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+char board[10000][10000];
+
+void merge(int y, int x, int size){
+	if(size==1){
+		board[y][x] = '*';
+		return;
+	}
+	
+	int nextSize = size/3;
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			if(i == 1 && j == 1) continue;
+			
+			merge(y+(i*nextSize), x+(j*nextSize), nextSize);
+		}
+	}
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	fill(&board[0][0], &board[0][0]+10000*10000, ' ');
+	cin >> N;
+	
+	merge(0, 0, N);
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			cout << board[i][j];
+		}
+		cout << "\n";
+	}
+}
