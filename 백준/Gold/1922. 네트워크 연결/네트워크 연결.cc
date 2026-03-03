fd3f8d682570d9d269a81b3d20d0af52af9d15eb
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-03 13:53:34 +0900

    [Gold IV] Title: 네트워크 연결, Time: 32 ms, Memory: 4464 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -1,18 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef tuple<int,int,int> tt;
 int N, M, a, b, c, ret, p[1004], level[1004];
-priority_queue <tt, vector<tt>, greater<tt>> pq;
+
+struct Edge{
+	int cost, a, b;
+	
+	bool operator<(const Edge &other) const{
+		return cost < other.cost;
+	}
+};
+vector <Edge> edges;
 
 int find(int x){
 	if(p[x]==-1) return x;
 	return p[x] = find(p[x]);
 }
 
-void merge(int x, int y){
+bool merge(int x, int y){
 	x = find(x); y = find(y);
-	if(x==y) return;
+	if(x==y) return false;
+	
 	if(level[x] < level[y]){
 		p[x] = y;
 	}
@@ -21,6 +29,7 @@ void merge(int x, int y){
 		if(level[x]==level[y])	
 			level[x]++;
 	}
+	return true;
 }
 
 int main(){
@@ -30,18 +39,18 @@ int main(){
 	cin >> M;
 	for(int i = 0; i < M; i++){
 		cin >> a >> b >> c;
-		pq.push(make_tuple(c, a, b));
+		edges.push_back({c, a, b});
 	}
+	sort(edges.begin(), edges.end());
+	int edge_count = 0;
 	
-	while(pq.size()){
-		auto f = pq.top(); pq.pop();
-		tie(c, a,b) = f;
-		int x = find(a), y = find(b);
-		if(x==y) continue;
-		//cout << c << " " << a << " " << b << "\n";
-
-		merge(x, y);
-		ret += c;
+	for(const auto & edge : edges){
+		if(merge(edge.a, edge.b)){
+			ret += edge.cost;
+			edge_count++;
+		}
+		
+		if(edge_count == N-1) break;
 	}
 	
 	cout << ret;
