a132db78e97c819911adbb74e754b967ea26b302
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-08 15:37:23 +0900

    [Platinum V] Title: 공장, Time: 160 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Platinum/7578. 공장/공장.cc
+++ 백준/Platinum/7578. 공장/공장.cc
@@ -3,45 +3,43 @@ using namespace std;
 
 typedef long long ll;
 typedef pair<int,int> pp;
-int N, a, A[500004], B[1000004], tree[2000004];
+int N, a, A[500004], B[1000004];
+ll tree[500004];
 vector <int> C;
 ll ret;
 
-int query(int s, int e, int node, int left, int right){
-	if(e < left || s > right) return 0;
-	if(left <= s && e <= right) return tree[node];
-	
-	int mid = (s+e)/2;
-	int l = query(s, mid, node*2, left, right);
-	int r = query(mid+1, e, node*2+1, left, right);
-	return l+r;
+void update(int idx, int value){
+	while(idx <= N){
+		tree[idx] += value;
+		idx += idx&-idx;
+	}
 }
 
-void update(int s, int e, int node, int target){
-	if(e < target || s > target) return;
-	tree[node]++;
-	if(s==e) return;
-	
-	int mid = (s+e)/2;
-	update(s, mid, node*2, target);
-	update(mid+1, e, node*2+1, target);
+ll sum(int idx){
+	ll ret = 0;
+	while(idx > 0){
+		ret += tree[idx];
+		idx -= idx&-idx;
+	}
+	return ret;
 }
+
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
-	for(int i = 0; i < N; i++){
+	for(int i = 1; i <= N; i++){
 		cin >> A[i];
 	}
-	for(int i = 0; i < N; i++){
+	for(int i = 1; i <= N; i++){
 		cin >> a;
 		B[a] = i;
 	}
 
-	for(int i = 0; i < N; i++){
+	for(int i = 1; i <= N; i++){
 		//1-based
-		ret += query(0, N-1, 1, B[A[i]]+1, N);
-		update(0, N-1, 1, B[A[i]]);
+		ret += sum(N) - sum(B[A[i]]);
+		update(B[A[i]], 1);
 	}
 	
 	cout << ret;
