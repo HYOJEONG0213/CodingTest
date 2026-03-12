6cb60a89963507344b1b40f30cb0270fb2c5bf82
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-12 21:34:42 +0900

    [Platinum IV] Title: 달리기, Time: 156 ms, Memory: 18064 KB -BaekjoonHub

--- 백준/Platinum/2517. 달리기/달리기.cc
+++ 백준/Platinum/2517. 달리기/달리기.cc
@@ -4,41 +4,54 @@ using namespace std;
 typedef pair<int,int> pp;
 const int MAXNUM = 1000000000;
 int N, A[500004], ret[500004], tree[500004];
-vector <pp> sortA;
+vector <pp> player, temp(500004);
 
-void update(int index, int value){
-	while(index <= N){
-		tree[index] += value;
-		index += index&-index;
+void mergeSort(int s, int mid, int e){
+	int i = s, j = mid+1, k = s;
+	while(i <= mid && j <= e){
+		if(player[i].first < player[j].first){
+			ret[player[j].second] += mid - i + 1;
+			temp[k++] = player[j++];
+		}
+		else{
+			temp[k++] = player[i++];
+		}
+	}
+	
+	while(i<=mid){
+		temp[k++] = player[i++];
+	}
+	while(j<=e){
+		temp[k++] = player[j++];
+	}
+	
+	for(int i = s; i <= e; i++){
+		player[i] = temp[i];
 	}
 }
 
-int sum(int index){
-	int num = 0;
-	while(index > 0){
-		num += tree[index];
-		index -= index&-index;
-	}
-	return num;
+
+void merge(int s, int e){
+	if(s >= e) return;
+	
+	int mid = (s+e)/2;
+	merge(s, mid);
+	merge(mid+1, e);
+	mergeSort(s, mid, e);
 }
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
+	player.push_back({0, 0});
 	for(int i = 1; i <= N; i++){
 		cin >> A[i];
-		sortA.push_back({A[i], i});
+		player.push_back({A[i], i});
 	}
-	sort(sortA.begin(), sortA.end());
 	
-	for(int i = 0; i < sortA.size(); i++){
-		int value = sortA[i].first;
-		int index = sortA[i].second;
-		ret[index] = index - sum(index);
-		update(index, 1);
-	}
+	merge(1, N);
 	
 	for(int i = 1; i <= N; i++){
-		cout << ret[i] << "\n";
+		cout << i - ret[i] << "\n";
 	}
 }
