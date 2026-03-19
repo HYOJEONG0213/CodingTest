f6b2ae388dbbbd639613615d940d3378bf3f2409
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-19 22:23:54 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 8 ms, Memory: 3320 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.cc
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.cc
@@ -2,8 +2,59 @@
 using namespace std;
 
 const int MAXLEVEL = 16;
-int T, N, A, B, parent[10004];
-bool visited[10004];
+int T, N, A, B, parent[10004][MAXLEVEL], visited[10004];
+vector <int> adj[10004];
+
+
+void bfs(){
+	int s;
+	for(int i = 1; i <= N; i++){
+		if(parent[i][0]==0){
+			s = i;
+			break;
+		}
+	}
+	
+	visited[s] = 1;
+	queue <int> q;
+	q.push(s);
+	while(q.size()){
+		int here = q.front(); q.pop();
+		for(int i : adj[here]){
+			if(visited[i]) continue;
+			visited[i] = visited[here]+1;
+			q.push(i);
+		}
+	}
+}
+
+void setParent(){
+	for(int j = 1; j < MAXLEVEL; j++){
+		for(int i = 1; i <= N; i++){
+			parent[i][j] = parent[parent[i][j-1]][j-1];
+		}
+	}
+}
+
+int getParent(int u, int v){
+	if(visited[u] < visited[v]) swap(u, v);
+	for(int j = MAXLEVEL-1; j >= 0; j--){
+		if(visited[u] - visited[v] >= 1<<j){
+			u = parent[u][j];
+		}
+	}
+	
+	if(u==v) return u;
+	
+	for(int j = MAXLEVEL-1; j >= 0; j--){
+		if(parent[u][j] != parent[v][j]){
+			u = parent[u][j];
+			v = parent[v][j];
+		}
+	}
+	
+	return parent[u][0];
+}
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -11,27 +62,23 @@ int main(){
 	while(T--){
 		memset(parent, 0, sizeof(parent));
 		memset(visited, 0, sizeof(visited));
+		for(int i = 0; i < 10004; i++){
+			adj[i].clear();
+		}
+			
+
 
 		cin >> N;
 		for(int i = 0; i < N-1; i++){
 			cin >> A >> B;
-			parent[B] = A;
+			adj[A].push_back(B);
+			adj[B].push_back(A);
+			parent[B][0] = A;
 		}
 		
+		bfs();
+		setParent();
 		cin >> A >> B;
-		
-		int here = A;
-		while(here != 0){
-			visited[here] = true;
-			here = parent[here];
-		}
-		here = B;
-		while(here != 0){
-			if(visited[here]){
-				cout << here << "\n";
-				break;
-			}
-			here = parent[here];
-		}
+		cout << getParent(A, B) << "\n";
 	}
 }
