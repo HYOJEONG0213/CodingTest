3f2774206abedc82065edda1c6e7e6641af6fff3
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-24 14:02:42 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 2880 ms, Memory: 163024 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -5,7 +5,7 @@ typedef long long ll;
 typedef pair<int,int> pp;
 int N, A[4004], B[4004], C[4004], D[4004];
 ll ret;
-vector<int> AB;
+vector<int> AB, CD;
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -18,19 +18,37 @@ int main(){
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < N; j++){
 			AB.push_back(A[i]+B[j]);
+			CD.push_back(C[i]+D[j]);
 		}
 	}
 	
 	sort(AB.begin(), AB.end());
+	sort(CD.begin(), CD.end());
 	
-	for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-        	int target = -(C[i]+D[j]);
-        	
-			auto lower = lower_bound(AB.begin(), AB.end(), target);
-			auto upper = upper_bound(AB.begin(), AB.end(), target);
+	int left = 0;
+	int right = CD.size()-1;
+	while(left < AB.size() && right >= 0){
+		int current = AB[left] + CD[right];
+		
+		if(current==0){
+			int target1 = AB[left], target2 = CD[right];
+			ll cnt1 = 0, cnt2 = 0;
+			while(left < AB.size() && AB[left] == target1){
+				cnt1++;
+				left++;
+			}
+			while(right >= 0 && CD[right] == target2){
+				cnt2++;
+				right--;
+			}
 			
-			ret += (upper-lower);
+			ret += cnt1 * cnt2;
+		}
+		else if(current < 0){
+			left++;
+		}
+		else{
+			right--;
 		}
 	}
 	
