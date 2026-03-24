26444ce187b9f01af2b3c0ecffe4dd3418f4467b
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-24 14:09:56 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 8556 ms, Memory: 631804 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -1,56 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int,int> pp;
-int N, A[4004], B[4004], C[4004], D[4004];
-ll ret;
-vector<int> AB, CD;
+int N;
+int A[4004], B[4004], C[4004], D[4004];
+long long ret;
+unordered_map<int, int> AB;
 
-int main(){
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> N;
-	AB.reserve(N*N);
-	
-	for(int i = 0; i < N; i++){
-		cin >> A[i] >> B[i] >> C[i] >> D[i];
-	}
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			AB.push_back(A[i]+B[j]);
-			CD.push_back(C[i]+D[j]);
-		}
-	}
-	
-	sort(AB.begin(), AB.end());
-	sort(CD.begin(), CD.end());
-	
-	int left = 0;
-	int right = CD.size()-1;
-	while(left < AB.size() && right >= 0){
-		int current = AB[left] + CD[right];
-		
-		if(current==0){
-			int target1 = AB[left], target2 = CD[right];
-			ll cnt1 = 0, cnt2 = 0;
-			while(left < AB.size() && AB[left] == target1){
-				cnt1++;
-				left++;
-			}
-			while(right >= 0 && CD[right] == target2){
-				cnt2++;
-				right--;
-			}
-			
-			ret += cnt1 * cnt2;
-		}
-		else if(current < 0){
-			left++;
-		}
-		else{
-			right--;
-		}
-	}
-	
-	cout << ret;
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> N;
+    for(int i = 0; i < N; i++) {
+        cin >> A[i] >> B[i] >> C[i] >> D[i];
+    }
+    
+    AB.reserve(N * N);
+    
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            AB[A[i] + B[j]]++;
+        }
+    }
+    
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            int target = -(C[i] + D[j]);
+            
+            if (AB.count(target)) {
+                ret += AB[target];
+            }
+        }
+    }
+    
+    cout << ret << "\n";
+    return 0;
 }
