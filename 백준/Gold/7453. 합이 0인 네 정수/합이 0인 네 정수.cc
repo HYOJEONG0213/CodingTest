b2e3143c1f0023bde0aff14297375dff5d0b501e
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-24 13:52:30 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 8988 ms, Memory: 64592 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -3,30 +3,35 @@ using namespace std;
 
 typedef long long ll;
 typedef pair<int,int> pp;
-ll N, A[4004], B[4004], C[4004], D[4004];
+int N, A[4004], B[4004], C[4004], D[4004];
 ll ret;
-vector<ll> AB, CD;
+vector<int> AB;
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
+	AB.reserve(N*N);
+	
 	for(int i = 0; i < N; i++){
 		cin >> A[i] >> B[i] >> C[i] >> D[i];
 	}
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < N; j++){
 			AB.push_back(A[i]+B[j]);
-			CD.push_back(C[i]+D[j]);
 		}
 	}
 	
-	sort(CD.begin(), CD.end());
+	sort(AB.begin(), AB.end());
 	
-	for(ll target : AB){
-		auto lower = lower_bound(CD.begin(), CD.end(), -target);
-		auto upper = upper_bound(CD.begin(), CD.end(), -target);
-		
-		ret += (upper-lower);
+	for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+        	int target = -(C[i]+D[j]);
+        	
+			auto lower = lower_bound(AB.begin(), AB.end(), target);
+			auto upper = upper_bound(AB.begin(), AB.end(), target);
+			
+			ret += (upper-lower);
+		}
 	}
 	
 	cout << ret;
