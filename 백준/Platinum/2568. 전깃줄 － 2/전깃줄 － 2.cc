f1c474edc87d56290f9b1a6c38348e3f2ad177a7
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-25 11:25:36 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 40 ms, Memory: 6540 KB -BaekjoonHub

---
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 987654321;
+typedef pair<int,int> pp;
+int N, a, b, B[100004], p[500004];
+vector <pp> A;
+vector <int> ret;
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> N;
+	for(int i = 0; i < N; i++){
+		cin >> a >> b;
+		A.push_back({b, a});
+	}
+	sort(A.begin(), A.end());
+	
+	int n = 0;
+ 	for(int i = 0; i < N; i++){
+		int value = A[i].second;
+		auto *iter = lower_bound(B, B+n, value);
+		if(*iter == 0){
+			n++;
+		}
+		*iter = value;
+		p[value] = (iter - B) + 1;
+	}
+	
+	cout << N-n << "\n";
+	for(int i = N-1; i >= 0; i--){
+		//cout << p[A[i].second] << " ";
+		
+		if(p[A[i].second]==n){
+			n--;
+		}
+		else{
+			ret.push_back(A[i].second);
+		}
+	}
+	
+	sort(ret.begin(), ret.end());
+	for(int i : ret){
+		cout << i << "\n";
+	}
+}
