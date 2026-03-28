d773e8598b979afa0928a858505655ddacd17388
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-28 14:31:06 +0900

    [Gold I] Title: 계단 수, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -4,32 +4,42 @@ using namespace std;
 typedef long long ll;
 const int MOD = 1000000000;
 int N;
-ll dp[104][10][(1<<10) + 4], ret;
+ll dp[2][10][(1<<10) + 4], ret;
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
 	
+	int here = 0;
 	for(int i = 1; i <= 9; i++){
-		dp[1][i][1<<i] = 1;
+		dp[here][i][1<<i] = 1;
 	}
 	for(int i = 2; i <= N; i++){
+		int next = 1 - here;
+		
+		memset(dp[next], 0, sizeof(dp[next]));
+		
 		for(int j = 0; j <= 9; j++){
 			for(int k = 0; k <= (1<<10)-1; k++){
-				int next = k | (1<<j);
+				if(dp[here][j][k]==0) continue;
+				
+				
 				
 				if(j > 0){
-					dp[i][j][next] = (dp[i][j][next] + dp[i-1][j-1][k]) % MOD;
+					int bit = k | (1<<(j-1));
+					dp[next][j-1][bit] = (dp[next][j-1][bit] + dp[here][j][k]) % MOD;
 				}
 				if(j < 9){
-					dp[i][j][next] = (dp[i][j][next] + dp[i-1][j+1][k]) % MOD;
+					int bit = k | (1<<(j+1));
+					dp[next][j+1][bit] = (dp[next][j+1][bit] + dp[here][j][k]) % MOD;
 				}
 			}
 		}
+		here = next;
 	}
 
 	for(int j = 0; j <= 9; j++){
-		ret = (ret + dp[N][j][(1<<10) - 1]) % MOD;
+		ret = (ret + dp[here][j][(1<<10) - 1]) % MOD;
 	}
 	cout << ret;
 	
