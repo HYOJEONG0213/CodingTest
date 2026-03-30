4ad68b42ccb67c37b081779f5623dc21b33d1e5c
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-03-30 22:54:23 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 40 ms, Memory: 9904 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
@@ -1,38 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, board[1004][1004], p[1004*1005], level[1004*1005], visited[1004*1005], ret;
+int N, M, board[1004][1004], visited[1004][1004], ret;
 int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
 string s;
 map <char, int> mp = {{'U', 0}, {'R', 1}, {'D', 2}, {'L', 3}};
 
-int find(int x){
-	if(p[x]==-1){
-		return x;
-	}
-	return p[x] = find(p[x]);
-}
-
-void merge(int x, int y){
-	x = find(x), y = find(y);
+void dfs(int y, int x){
+	visited[y][x] = 1;
 	
-	if(x==y) return;
+	int ny = y + dy[board[y][x]];
+	int nx = x + dx[board[y][x]];
+	if(visited[ny][nx]==0) {
+		dfs(ny, nx);
+	}
+	else if(visited[ny][nx]==1){
+		ret++;
+	}
 	
-	if(level[x] < level[y]){
-		p[x] = y;
-	}
-	else{
-		if(level[x] == level[y]){
-			level[x]++;
-		}
-		p[y] = x;
-	}
+	visited[y][x] = 2;
 }
 
 int main(){
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	memset(p, -1, sizeof(p));
-	
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);	
 	cin >> N >> M;
 	for(int i = 0; i < N; i++){
 		cin >> s;
@@ -43,24 +33,12 @@ int main(){
 	
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < M; j++){
-			int idx = M * i + j;
-			int temp = find(idx);
-			if(p[temp] != -1) continue;
+			if(visited[i][j]==0) {
+				dfs(i, j);
+			}
 			
-			int ny = i + dy[board[i][j]];
-			int nx = j + dx[board[i][j]];
-			merge(idx, ny * M + nx);
+			dfs(i, j);
 		}
-	}
-	
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < M; j++){
-			int idx = M * i + j;
-			int temp = find(idx);
-			if(visited[temp] != 0) continue;
-			visited[temp] = 1;
-			ret++;
-		} 
 	}
 	
 	cout << ret;
