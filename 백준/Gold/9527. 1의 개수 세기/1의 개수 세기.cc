bb1e547ee4dabdb5428656affb0205d3972ffea9
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-02 15:28:59 +0900

    [Gold II] Title: 1의 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
@@ -2,22 +2,30 @@
 using namespace std;
 
 typedef long long ll;
-ll A, B;
+ll A, B, dp[60];
+
+void init(){
+	dp[0] = 0;
+	for(int i = 1; i < 60; i++){
+		dp[i] = dp[i-1] * 2 + (1LL << (i-1));
+	}
+}
 
 ll f(ll N){
-	ll ret = 0;
-	for(ll i = 0; (1LL<<i) <= N; i++){
-		ll cycle = (1LL<<(i+1));
-		ret += ((N+1) / cycle) * (1LL<<i);
-		
-		if((N+1) % cycle > 0)
-			ret += max(0LL, (N+1) % cycle - (1LL<<i) );
-	} 
-	return ret;
+	if(N <= 0) return 0;
+	//if(N == 1) return 1;
+	
+	ll i = 0;	// 최상위 비트 i 
+	for(i = 59; i >= 0; i--){
+		if(N & (1LL<<i)) break;
+	}
+	
+	return dp[i] + (N - (1LL<<i) + 1) + f(N - (1LL << i));
 }
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	init();
 	cin >> A >> B;
 	cout << f(B) - f(A-1);
 }
