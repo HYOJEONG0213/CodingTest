4aac85d4e48dbedaa70dbe12a58b67b12c6a640f
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-04 14:54:35 +0900

    [Gold III] Title: 두 배열의 합, Time: 524 ms, Memory: 61968 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int T, n, m, A[1004], B[1004], psumA[1004], psumB[1004];
+ll ret;
+map <int,int> mpA, mpB;
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> T;
+	cin >> n;
+	for(int i = 1; i <= n; i++){
+		cin >> A[i];
+		psumA[i] = psumA[i-1]+A[i];
+	}
+	cin >> m;
+	for(int i = 1; i <= m; i++){
+		cin >> B[i];
+		psumB[i] = psumB[i-1]+B[i];
+	}
+	for(int i = 1; i <= n; i++){
+		for(int j = i; j <= n; j++){
+			int num = psumA[j] - psumA[i-1];
+			mpA[num]++;
+		}
+	}
+	for(int i = 1; i <= m; i++){
+		for(int j = i; j <= m; j++){
+			int num = psumB[j] - psumB[i-1];
+			mpB[num]++;
+		}
+	}
+	
+	for(auto iter = mpA.begin(); iter != mpA.end(); iter++){
+		ret += (ll)iter->second * mpB[T-iter->first];
+	}
+	
+	cout << ret;
+}
