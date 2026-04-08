d1c29723f39b8a8ae87237304b6e9992e7e7a46e
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-08 16:00:27 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 44 ms, Memory: 6316 KB -BaekjoonHub

---
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, x[100004], dp[1000004];
+const int INF = 987654321;
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	fill(dp, dp+1000004, INF);
+	cin >> N;
+	for(int i = 0; i < N; i++) {
+		cin >> x[i];
+		dp[x[i]]=0;
+	}
+	for(int i = 0; i < N; i++) {
+		for(int j = x[i]*2; j <= 1000000; j+=x[i]){
+			if(dp[j] == INF) continue;
+			dp[x[i]]++;
+			dp[j]--;
+		}
+	}
+	
+	for(int i = 0; i < N; i++) {
+		cout << dp[x[i]] << " ";	
+	}
+}
