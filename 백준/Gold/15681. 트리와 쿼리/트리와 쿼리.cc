41bdb221aa5764bcb7597ec4322eb2c221295b16
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-13 00:07:51 +0900

    [Gold V] Title: 트리와 쿼리, Time: 56 ms, Memory: 12872 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -2,12 +2,21 @@
 using namespace std;
 
 typedef pair<int,int> pp;
-int N, R, Q, a, b, p[100004], arr[100004], psum[100004], visited[100004];
+int N, R, Q, a, b, psum[100004], visited[100004];
 vector <int> adj[100004];
 
+void f(int here){
+	visited[here] = 1;
+	for(auto i : adj[here]){
+		if(visited[i]) continue;
+		f(i);
+		psum[here] += psum[i];
+	}
+}
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	fill(psum, psum+100004, 1);
 	cin >> N >> R >> Q;
 	for(int i = 0; i < N-1 ; i++){
 		cin >> a >> b;
@@ -15,35 +24,7 @@ int main(){
 		adj[b].push_back(a);
 	}
 	
-	queue <int> q;
-	q.push(R);
-	visited[R] = 1;
-	while(q.size()){
-		int here = q.front(); q.pop();
-		for(int i : adj[here]){
-			if(visited[i]) continue;
-			visited[i]=1;
-			p[i] = here;
-			q.push(i);
-			arr[here]++;
-		}
-	}
-	
-	for(int i = 1; i <= N; i++){
-		if(arr[i]==0){
-			q.push(i);
-		}
-		psum[i]=1;
-	}
-	
-	while(q.size()){
-		int here = q.front(); q.pop();
-		//cout << here << " ";
-		arr[p[here]]--;
-		psum[p[here]] += psum[here];
-		if(here==R) continue;
-		if(arr[p[here]]==0) q.push(p[here]);
-	}
+	f(R);
 	
 	for(int i = 0; i < Q; i++){
 		cin >> a;
