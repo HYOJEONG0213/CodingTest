17b2a4e8c6d4fcc50a0dc83aede8961ba54dfc73
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-14 14:17:47 +0900

    [Gold I] Title: 배열 정렬, Time: 100 ms, Memory: 4800 KB -BaekjoonHub

--- 백준/Gold/28707. 배열 정렬/배열 정렬.cc
+++ 백준/Gold/28707. 배열 정렬/배열 정렬.cc
@@ -2,56 +2,78 @@
 using namespace std;
 
 typedef long long ll;
-typedef array<int, 14> aa;
-typedef pair<ll, aa> pp;
-int N, M, l[14], r[14], c[14];
-array <int, 14> A;
+typedef pair<ll, ll> pp;
+int N, M, l[14], r[14], c[14], a;
 priority_queue <pp, vector<pp>, greater<pp>> pq;
-map <aa, ll> mp;
+vector <int> A, uniA, sortA;
+map <ll, ll> mp;
 
-bool check(array<int, 14> C){
-	for(int i = 1; i < N; i++){
-		if(C[i] > C[i+1]) return false;
-	}
-	return true;
+ll pack(const vector <int> &v){
+	ll res = 0;
+	for(int i = 1; i <= N; i++) res = res*10 + v[i];
+	return res;
 }
+
+vector <int> unpack(ll state){
+	vector <int> v(N+1);
+	for(int i = N; i > 0; i--){
+		v[i] = state % 10;
+		state /= 10;
+	}
+	return v;
+}
+
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
+	A.resize(N+1);
 	for(int i = 1; i <= N; i++){
 		cin >> A[i];
+		uniA.push_back(A[i]);
 	}
+	
+	sort(uniA.begin(), uniA.end());
+	uniA.erase(unique(uniA.begin(), uniA.end()), uniA.end());
+	for(int i = 1; i <= N; i++){
+		A[i] = lower_bound(uniA.begin(), uniA.end(), A[i]) - uniA.begin();
+	}
+	
+	sortA = A;
+	sort(sortA.begin()+1, sortA.end());
+	ll target = pack(sortA);
+	
 	cin >> M;
 	for(int i = 1; i <= M; i++){
 		cin >> l[i] >> r[i] >> c[i];
 	}
 	
-	mp[A] = 0;
+	ll s = pack(A);
+	mp[s] = 0;
 	
-	pq.push({0, A});
+	pq.push({0, s});
 	while(pq.size()){
 		auto f = pq.top(); pq.pop();
-		ll herecost;
-		aa B;
-		tie(herecost, B) = f;
+		ll herecost, here;
+		tie(herecost, here) = f;
 		
-		if(check(B)==true){
+		if(here==target){
 			cout << herecost;
 			return 0;
 		}
 		
-		if(mp[B] < herecost) continue;
+		if(mp[here] < herecost) continue;
 		
-		//i : 이번에 선택, j : 이전값 
+		vector <int> v = unpack(here);
 		for(int i = 1; i <= M; i++){
-			aa tempB = B;
-			swap(tempB[l[i]], tempB[r[i]]);
-			ll next = herecost + c[i];
+			vector <int> vv = v;
+			swap(vv[l[i]], vv[r[i]]);
+			ll next = pack(vv);
+			ll nextcost = herecost + c[i];
 			
-			if(mp[tempB]==0 || mp[tempB] > next){
-				mp[tempB] = next;
-				pq.push({herecost+c[i], tempB});
+			if(mp[next]==0 || mp[next] > nextcost){
+				mp[next] = nextcost;
+				pq.push({herecost+c[i], next});
 			}
 		}
 	}
