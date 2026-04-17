97cee0052869390c4f59af7480a9252196e0bf63
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-17 21:08:54 +0900

    [Platinum III] Title: 열혈강호 4, Time: 304 ms, Memory: 4164 KB -BaekjoonHub

---
+++ 백준/Platinum/11378. 열혈강호 4/열혈강호 4.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M, K, a, b, visited[1004], assigned[1004], ret;
+vector <int> person[1004];
+
+bool f(int idx){
+	for(int i : person[idx]){
+		if(visited[i]) continue;
+		visited[i] = true;
+		
+		if(assigned[i]==-1 || f(assigned[i])){
+			assigned[i] = idx;
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	memset(assigned, -1, sizeof(assigned));
+	cin >> N >> M >> K;
+	for(int i = 0; i < N; i++){
+		cin >> a;
+		for(int j = 0; j < a; j++){
+			cin >> b;
+			person[i].push_back(b);
+		}
+	}
+	
+	for(int i = 0; i < N; i++){
+		memset(visited, 0, sizeof(visited));
+		if(f(i)){
+			ret++;
+		}
+	}
+	
+	int cnt = 0;
+	for(int i = 0; i < N; i++){
+		memset(visited, 0, sizeof(visited));
+		while(f(i)){
+			ret++;
+			cnt++;
+			memset(visited, 0, sizeof(visited));
+			
+			if(cnt == K) break;
+		}
+		
+		if(cnt == K) break;
+	}
+	
+	cout << ret;
+}
