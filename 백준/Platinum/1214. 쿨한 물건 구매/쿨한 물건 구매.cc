b66b8221dba7fd575131330aba9e50121fd806bf
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-18 19:30:36 +0900

    [Platinum V] Title: 쿨한 물건 구매, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.cc
+++ 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.cc
@@ -1,12 +1,33 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll D, P, Q, mn=1e18;
+
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> D >> P >> Q;
+	if(P > Q) swap(P, Q);
+	
+	// Q, P의 최대 횟수 : q, p 
+	ll q = (D/Q +1);
+	ll maxcycle = P;
+	
+	for(ll i = 0; i <= min(q, maxcycle); i++){
+		ll remain = D - Q*i;
+		
+        // D이상 커지면 ㅈㅈ 
+		if(remain <= 0) {
+			mn = min(mn, Q*i);
+			break;
+		}
+		
+		ll pcnt = (remain + P-1) / P;
+		ll sum = Q*i + P*pcnt;
+		mn = min(mn, sum);
+		if(mn==D) break;
+	}
+	
+	cout << mn;
+}
