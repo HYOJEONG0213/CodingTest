5a9b4857f67360c9123f0b029b4237b6974b572e
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-19 17:23:01 +0900

    [Platinum IV] Title: 컨닝, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Platinum/1014. 컨닝/컨닝.cc
+++ 백준/Platinum/1014. 컨닝/컨닝.cc
@@ -1,29 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int T, N, M, visited[150], assigned[104], ret;
+int T, N, M, c[150][150], f[150][150], ret, S = 120, E = 121, maxflow, p[150];
 int dy[6] = {-1, 0, 1, -1, 0, 1}, dx[6] = {-1, -1, -1, 1, 1, 1};
 string s;
 char board[14][14];
 
-vector <int> L, R[150];
+vector <int> adj[150];
 
 int getNum(int y, int x){
 	return y*M + x;
 }
 
-bool dfs(int idx){
-	// idx: L 노드 , i : R노드  
-	for(int i : R[idx]){
-		if(visited[i]) continue;
-		visited[i] = 1;
-		if(assigned[i]==-1 || dfs(assigned[i])){
-			assigned[i] = idx;
-			return true;
+void bfs(){
+	while(1){
+		memset(p, -1, sizeof(p));
+		queue <int> q;
+		q.push(S);
+		
+		while(q.size()){
+			int here = q.front(); q.pop();
+			for(int i : adj[here]){
+				if(c[here][i] - f[here][i] > 0 && p[i] == -1){
+					q.push(i);
+					p[i] = here;
+				}
+			}
+			if(p[E] != -1) break;
 		}
+		
+		if(p[E]==-1) break;
+		
+		for(int here = E; here != S; here = p[here]){
+			int pre = p[here];
+			f[pre][here]++;
+			f[here][pre]--;
+		}
+		maxflow++;
 	}
-	
-	return false;
 }
 
 int main(){
@@ -31,11 +45,12 @@ int main(){
 	cin >> T;
 	while(T--){	
 		// 초기화 필요  
-		memset(assigned, -1, sizeof(assigned));
-		L.clear();
 		ret = 0;
+		maxflow = 0;
+		memset(c, 0, sizeof(c));
+		memset(f, 0, sizeof(f));
 		for(int i = 0; i < 150; i++){
-			R[i].clear();
+			adj[i].clear();
 		}
 		
 		cin >> N >> M;
@@ -44,31 +59,43 @@ int main(){
 			for(int j = 0; j < M; j++){
 				board[i][j] = s[j];
 				if(board[i][j]=='.'){
-					if(j % 2 == 0)
-						L.push_back(getNum(i, j));
 					ret++;
+					if(j % 2 == 0){
+						adj[S].push_back(getNum(i, j));
+						adj[getNum(i, j)].push_back(S);
+						c[S][getNum(i, j)] = 1;
+					}
+					else{
+						adj[getNum(i, j)].push_back(E);
+						adj[E].push_back(getNum(i, j));
+						c[getNum(i, j)][E] = 1;
+					}	
+					
 				}
 			}
 		}
 		
-		for(int i : L){
-			int y, x;
-			y = i/M; x = i%M;
-			for(int j = 0; j < 6; j++){
-				int ny = y + dy[j], nx = x + dx[j];
-				if(ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-				if(board[ny][nx]=='x') continue;
-				R[i].push_back(getNum(ny, nx));
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j < M; j+=2){
+				if(board[i][j]=='x') continue;
+				
+				int here = getNum(i, j);
+				for(int k = 0; k < 6; k++){
+					int ny = i + dy[k], nx = j + dx[k];
+					if(ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+					if(board[ny][nx]=='x') continue;
+					
+					// 짝수열에서 홀수열 가는거 만들기 
+					int next = getNum(ny, nx);
+					adj[here].push_back(next);
+					adj[next].push_back(here);
+					c[here][next] = 1; 
+				}
 			}
 		}
 		
-		for(int i : L){
-			memset(visited, 0, sizeof(visited));
-			if(dfs(i)){
-				ret--;
-			} 
-		}
+		bfs();
 		
-		cout << ret << "\n";
+		cout << ret - maxflow<< "\n";
 	}
 }
