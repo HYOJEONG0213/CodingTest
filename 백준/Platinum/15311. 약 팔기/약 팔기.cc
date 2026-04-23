e7aaa2ed843cb5429f3f6e6cd74e4bb72693b0b7
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-23 21:04:10 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	
+	cout << 2000 << "\n";
+	
+	for(int i = 0; i < 1000; i++){
+		cout << 1 << " ";
+	}
+	
+	for(int i = 0; i < 1000; i++){
+		cout << 1000 << " ";
+	}
+}
