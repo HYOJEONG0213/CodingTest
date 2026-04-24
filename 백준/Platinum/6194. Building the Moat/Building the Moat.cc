975156608d196637a1483c4cd1f8be1ef6d8a99a
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-24 23:58:16 +0900

    [Platinum V] Title: Building the Moat, Time: 0 ms, Memory: 2240 KB -BaekjoonHub

---
+++ 백준/Platinum/6194. Building the Moat/Building the Moat.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+struct Dot{
+	ll x, y;
+	
+	bool operator<(const Dot &other) const{
+		if(y == other.y) return x < other.x;
+		return y < other.y;
+	}
+};
+
+Dot pivot;
+ll N;
+double ret;
+stack <Dot> stk;
+
+
+ll getccw(Dot &a, Dot &b, Dot &c){
+	return a.x*b.y + b.x*c.y + c.x*a.y - (a.x*c.y + c.x*b.y + b.x*a.y);
+}
+
+ll getdist(Dot &a, Dot &b){
+	return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
+}
+
+bool compare(Dot &a, Dot &b){
+	ll ccw = getccw(pivot, a, b);
+	if(ccw != 0) return ccw < 0;	//시계 ㄱㄱ
+	return getdist(pivot, a) < getdist(pivot, b); 
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> N;
+	vector <Dot> dot(N);
+	for(int i = 0; i < N; i++){
+		cin >> dot[i].x >> dot[i].y;
+	}
+	
+	sort(dot.begin(), dot.end());
+	pivot = dot[0];
+	
+	sort(dot.begin()+1, dot.end(), compare);
+	
+	// 짧은 경로 = 볼록다각형!
+	stk.push(dot[0]);
+	stk.push(dot[1]);
+	for(int i = 2; i < dot.size(); i++){
+		while(stk.size() >= 2){
+			Dot second = stk.top(); stk.pop();
+			Dot first = stk.top(); 
+			
+			if(getccw(first, second, dot[i]) < 0){	//반시계 방향 
+				stk.push(second);
+				break;
+			}
+		}
+		stk.push(dot[i]);
+	}
+	
+	stk.push(pivot);
+	vector <Dot> v;
+	while(stk.size()){
+		v.push_back(stk.top());
+		stk.pop();
+	}
+	
+	for(int i = 0; i < v.size()-1; i++){
+		ret += sqrt(getdist(v[i], v[i+1]));
+	}
+	
+	cout << fixed << setprecision(2) << ret;
+}
