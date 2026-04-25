bb8692d7a3cb1d04ed012f1cfee743e72bfab28f
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-25 15:28:08 +0900

    [Platinum III] Title: 로버트 후드, Time: 36 ms, Memory: 3736 KB -BaekjoonHub

---
+++ 백준/Platinum/9240. 로버트 후드/로버트 후드.cc
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll C;
+
+struct Dot{
+	ll x, y;
+	// 왼쪽 아래에서 시작해서 반시계 방향으로 할거임  
+	bool operator <(const Dot &other) const{
+		if(y==other.y) return x < other.x;
+		return y < other.y;
+	}
+};
+
+Dot pivot;
+stack <Dot> stk;
+ll mx;
+
+ll getccw(Dot &a, Dot &b, Dot &c){
+	return a.x*b.y + b.x*c.y + c.x*a.y - (a.x*c.y + c.x*b.y + b.x*a.y);
+}
+
+ll getdist(Dot &a, Dot &b){
+	return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
+}
+
+bool compare(Dot &a, Dot &b){
+	ll ccw = getccw(pivot, a, b);
+	if(ccw != 0) return ccw > 0; // 반시계!! 
+	return getdist(pivot, a) < getdist(pivot, b);
+}
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> C;
+	vector <Dot> dot(C);
+	for(int i = 0; i < C; i++){
+		cin >> dot[i].x >> dot[i].y;
+	}
+	sort(dot.begin(), dot.end());
+	pivot = dot[0];
+	sort(dot.begin()+1, dot.end(), compare);
+	
+	stk.push(dot[0]);
+	stk.push(dot[1]);
+	for(int i = 2; i < dot.size(); i++){
+		while(stk.size() >= 2){
+			Dot second = stk.top(); stk.pop();
+			Dot first = stk.top();
+			if(getccw(first, second, dot[i]) > 0){
+				stk.push(second);
+				break;
+			}
+		}
+		stk.push(dot[i]);
+	}
+	
+	vector <Dot> v;
+	while(stk.size()){
+		v.push_back(stk.top()); stk.pop();
+	}
+	
+	for(int i = 0; i < v.size(); i++){
+		for(int j = i+1; j < v.size(); j++){
+			mx = max(mx, getdist(v[i], v[j]));
+		}
+	}
+	double ret = (double) mx;
+	cout << fixed << setprecision(6) << sqrt(ret);
+}
