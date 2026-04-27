144990b630d92c98a715954b119438fc8ae60c7f
HYO JEONG YANG <88478785+HYOJEONG0213@users.noreply.github.com>
2026-04-28 00:06:54 +0900

    [Platinum V] Title: 행운쿠키 제작소, Time: 616 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Platinum/10982. 행운쿠키 제작소/행운쿠키 제작소.cc
+++ 백준/Platinum/10982. 행운쿠키 제작소/행운쿠키 제작소.cc
@@ -4,37 +4,42 @@ using namespace std;
 const int INF = 987654321;
 const int TIME= 100000;
 int T, N, a, b;
+// 0 : 현재, 1 : next  
+int dp[2][TIME+4];
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> T;
 	while(T--){
 		cin >> N;
-		vector <int> dp(TIME+4, INF);
-		dp[0] = 0;
-
 		
+		fill(&dp[0][0], &dp[0][0]+(TIME+4)*2, INF);
+		dp[0][0] = 0;
+		
+		int cur = 0;
+		int next = 1;
+		int sum = 0;
+
 		for(int i = 0; i < N; i++){
 			cin >> a >> b;
+			sum += a;
+			fill(&dp[next][0], &dp[next][0]+TIME+4, INF);
 			
-			// 반죽 하나당 하나만 넣어야하므로  
-			vector <int> temp(TIME+4, INF);
-			
-			for(int j = 0; j <= TIME; j++){
-				if(dp[j]==INF) continue;
+			for(int j = 0; j <= sum; j++){
+				if(dp[cur][j]==INF) continue;
 				
 				// 오븐 a에 넣기
 				if(j + a <= TIME)
-					temp[j+a] = min(temp[j+a], dp[j]);
+					dp[next][j+a] = min(dp[next][j+a], dp[cur][j]);
 				// 오븐 b에 넣기  
-				temp[j] = min(temp[j], dp[j]+b);
+				dp[next][j] = min(dp[next][j], dp[cur][j]+b);
 			}
-			dp = temp;
+			swap(cur, next);
 		}
 		
 		int ret = INF;
 		for(int i = 0; i <= TIME; i++){
-			ret = min(ret, max(i, dp[i]));
+			ret = min(ret, max(i, dp[cur][i]));
 		}
 		cout << ret << "\n";
 	}
